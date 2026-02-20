c27a0e9e0707c016138583b77b26df5584de8397
mycampushub <mycampuslifestyle@gmail.com>
2026-02-21 00:24:11 +0600

    auth and middleware build errors fix

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
 import type { NextRequest } from 'next/server'
-import { gatewayAuthMiddleware } from './auth-middleware'
+import { gatewayAuthMiddleware } from './src/lib/api/auth-middleware'
 
 export function middleware(request: NextRequest) {
   return gatewayAuthMiddleware(request)
-}
-
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ],
 }

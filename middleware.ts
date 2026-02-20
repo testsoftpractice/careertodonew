8fe696bf3eb08db48a8a63828fb849a330fa934b
mycampushub <mycampuslifestyle@gmail.com>
2026-02-21 00:07:05 +0600

    middleware path changed

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
 }
